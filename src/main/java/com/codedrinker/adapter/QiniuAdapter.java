9601bf4c279bdc8e8360a670de11cdc159951b81
codedrinker <gwangchunlei@gmail.com>
2018-11-21 22:00:36 +0800

    添加日志

--- src/main/java/com/codedrinker/adapter/QiniuAdapter.java
+++ src/main/java/com/codedrinker/adapter/QiniuAdapter.java
@@ -1,5 +1,6 @@
 package com.codedrinker.adapter;
 
+import com.alibaba.fastjson.JSON;
 import com.codedrinker.dto.QiniuFile;
 import com.codedrinker.error.ErrorCode;
 import com.codedrinker.exception.ErrorCodeException;
@@ -11,6 +12,7 @@ import com.qiniu.storage.Configuration;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,7 @@ import java.util.UUID;
  * Created by codedrinker on 2018/10/12.
  */
 @Service
+@Slf4j
 public class QiniuAdapter {
 
     @Value("${qiniu.accessKey}")
@@ -42,21 +45,23 @@ public class QiniuAdapter {
         if (files.length < 2) {
             throw new ErrorCodeException(ErrorCode.UPLOAD_FAIL);
         }
-        String ext = files[files.length-1];
+        String ext = files[files.length - 1];
         String key = path + StringUtils.substring(UUID.randomUUID().toString(), 0, 10) + "." + ext;
         ByteArrayInputStream byteInputStream = new ByteArrayInputStream(qiniuFile.getUploadBytes());
         String token = getToken();
         try {
             Response response = uploadManager.put(byteInputStream, key, token, null, null);
             //解析上传成功的结果
+            log.info("qiniu upload info -> {}", JSON.toJSONString(response));
             DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
             return putRet.key;
         } catch (QiniuException ex) {
             Response r = ex.response;
-            System.err.println(r.toString());
+            log.info("qiniu upload : {}", r.toString());
             try {
                 System.err.println(r.bodyString());
             } catch (QiniuException ex2) {
+                log.error("qiniu upload error", ex2);
                 throw new ErrorCodeException(ErrorCode.UPLOAD_FAIL);
             }
         }
